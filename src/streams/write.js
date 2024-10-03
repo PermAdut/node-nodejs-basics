6a99f890e266772120183df16a9db422b6c15315
вфывфы <hdvhdvwww@gmail.com>
2024-10-03 21:28:02 +0300

    feat: done write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,16 @@
+import { createWriteStream } from "fs";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
 const write = async () => {
-    // Write your code here 
+  // Write your code here
+  // implement function that writes process.stdin data into file fileToWrite.txt content using Writable Stream
+  const __filename = fileURLToPath(import.meta.url);
+  const filePath = path.resolve(dirname(__filename), "files", "fileToWrite.txt");
+  const writeStream = createWriteStream(filePath);
+  process.stdin.once("data", (data) => {
+    writeStream.write(data.toString());
+  });
 };
 
 await write();
