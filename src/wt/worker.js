6977054f1b46e369ae32c97ed2011917e5491f65
вфывфы <hdvhdvwww@gmail.com>
2024-10-07 18:09:19 +0300

    feat: done worker.js

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,23 @@
+import worker from "node:worker_threads"
 // n should be received from main thread
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
     // This function sends result of nthFibonacci computations to main thread
+    // worker.js - extend given function to work with data received from main thread and implement function which sends result of the computation to the main thread
+    const n = process.argv[2];
+    const res = {data:null, status:"error"};
+    try{
+        const fib = nthFibonacci(n);
+        res.data = fib;
+        res.status = "resolved";
+    } catch(e){
+        res.data = null;
+        res.status = "error";
+    }
+    finally{
+        worker.parentPort.postMessage(res)
+    }
 };
 
 sendResult();
