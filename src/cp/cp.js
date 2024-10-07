332e9050f1e938f3c2e310db40c8f730486fe984
вфывфы <hdvhdvwww@gmail.com>
2024-10-07 20:30:21 +0300

    feat: done cp.js

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,6 @@
 import cp from "node:child_process"
-
+import path from "node:path"
+import { fileURLToPath } from "node:url";
 
 const spawnChildProcess = async (args) => {
     // Write your code here
@@ -7,8 +8,16 @@ const spawnChildProcess = async (args) => {
     // passing these args to it. This function should create IPC-channel between stdin and stdout of master process and child process:
     // child process stdin should receive input from master process stdin
     // child process stdout should send data to master process stdout
-    
+    const fileName = path.resolve(path.dirname(fileURLToPath(import.meta.url)),"files","script.js");
+    const childProcess = cp.fork(fileName, args);
+    childProcess.on('close', (code) => {
+        console.log(`Child Proc terminated process with code ${code}`)
+    })
+
+    childProcess.on('error', (error) => {
+        console.log(error)
+    })
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["hello", 1, "goodbye", "p", "uber"]);
